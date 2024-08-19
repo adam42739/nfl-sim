2889982f45a9fb33c47afd9430d5a37804eea433
Adam Lynch <adamclynch03@gmail.com>
2024-08-19 16:20:29 -0700

    added and completed data module

---
+++ src/data.py
@@ -0,0 +1,23 @@
+import nfl_data_py
+import datetime
+
+
+def this_year():
+    today = datetime.datetime.today()
+    if today.month < 6:
+        return today.year - 1
+    else:
+        return today.year
+
+
+THIS_YEAR = this_year()
+
+CACHE_PATH = "pbp_cache/"
+
+
+def cache(years=[THIS_YEAR]):
+    nfl_data_py.cache_pbp(years, downcast=True, alt_path=CACHE_PATH)
+
+
+def get(years):
+    return nfl_data_py.import_pbp_data(years, downcast=True, cache=True, alt_path=CACHE_PATH)
