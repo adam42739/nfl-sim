95aa9965c7c70bf218164ff545a1fba9051bf706
Adam Lynch <adamclynch03@gmail.com>
2024-08-22 17:04:48 -0700

    Updated README.md

--- src/data.py
+++ src/data.py
@@ -14,10 +14,14 @@ THIS_YEAR = this_year()
 
 CACHE_PATH = "pbp_cache/"
 
-
 def cache(years=[THIS_YEAR]):
     nfl_data_py.cache_pbp(years, downcast=True, alt_path=CACHE_PATH)
 
 
 def get(years):
     return nfl_data_py.import_pbp_data(years, downcast=True, cache=True, alt_path=CACHE_PATH)
+
+df = get([2023])
+new = df[["time_to_throw", "sack", "was_pressure"]]
+new = new[new["sack"]==1]
+print(new.head(10))
---
+++ src/simulation.py
@@ -0,0 +1,18 @@
+import data
+
+# p pass
+# p rushs
+#
+# pass
+#
+# p rc1 rc2 ...
+# p comp
+# 
+# dist time | comp & yards
+#
+# rush
+# p rs1 rs2 ...
+#
+# dist rush yrds
+# dist time | comp & yards
+#
