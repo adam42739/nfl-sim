51d204e65ebdb5063c7db5d6d47deb36c8c58015
Adam <adamclynch03@gmail.com>
2024-08-30 17:17:24 -0400

    edited src/data to fix outdated used of numpy.float_ in nfl_data_py

--- src/data.py
+++ src/data.py
@@ -1,4 +1,14 @@
 import nfl_data_py
+
+####################################
+# define numpy.float_ to fix
+# outdated use of it in nfl_data_py
+####################################
+import numpy
+
+numpy.float_ = numpy.float64
+####################################
+
 import datetime
 
 
@@ -14,14 +24,16 @@ THIS_YEAR = this_year()
 
 CACHE_PATH = "pbp_cache/"
 
+
 def cache(years=[THIS_YEAR]):
-    nfl_data_py.cache_pbp(years, downcast=True, alt_path=CACHE_PATH)
+    nfl_data_py.cache_pbp(years, alt_path=CACHE_PATH)
 
 
 def get(years):
-    return nfl_data_py.import_pbp_data(years, downcast=True, cache=True, alt_path=CACHE_PATH)
+    return nfl_data_py.import_pbp_data(years, cache=True, alt_path=CACHE_PATH)
+
 
 df = get([2023])
 new = df[["time_to_throw", "sack", "was_pressure"]]
-new = new[new["sack"]==1]
+new = new[new["sack"] == 1]
 print(new.head(10))
