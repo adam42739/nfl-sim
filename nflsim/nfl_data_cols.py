a113ac5e91dfc645b00a921e0a7b2af09670a8b3
Adam <adamclynch03@gmail.com>
2024-09-01 16:59:57 -0400

    feat: column names as build in global variables

---
+++ nflsim/nfl_data_cols.py
@@ -0,0 +1,28 @@
+class COLS:
+    class SCHEDULES:
+        GSIS = "gsis"
+        SEASON = "season"
+        WEEK = "week"
+        GAME_TYPE = "game_type"
+        GAMEDAY = "gameday"
+        WEEKDAY = "weekday"
+        GAMETIME = "gametime"
+        HOME_TEAM = "home_team"
+        AWAY_TEAM = "away_team"
+        HOME_REST = "home_rest"
+        AWAY_REST = "away_rest"
+        LOCATION = "location"
+
+    class ROSTERS:
+        TEAM = "team"
+        SEASON = "season"
+        WEEK = "week"
+        PLAYER_ID = "player_id"
+
+    class TEAMS:
+        ABBR = "team_abbr"
+        NAME = "team_name"
+        ID = "team_id"
+        CONF = "team_conf"
+        DIVISION = "team_division"
+
